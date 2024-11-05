d7ce9919b83c3bb5e485f13f98ee177d0aaae798
tjtldus <siyeon513@gmail.com>
2024-11-05 18:33:55 +0900

    Add files via upload

---
+++ tester.py
@@ -0,0 +1,11 @@
+import geo.utils as utils
+
+# calculate the length of hypotenuse(c) when a=3 and b=4
+a, b = 3, 4
+c = utils.pythagoras(3, 4)
+print('c =',c)
+
+# calculate the area of circle with radius r = 10
+r = 10
+area = utils.circle(10)
+print('area =',area)
